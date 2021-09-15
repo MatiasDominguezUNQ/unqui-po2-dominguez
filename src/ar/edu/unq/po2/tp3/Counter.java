3954c9de5a6e36b1e30343dbab6b201e02c968d6
Matias Dominguez <dmzmatias32@gmail.com>
2021-09-15 19:53:30 -0300

    Segundo Ejercicio

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -11,41 +11,6 @@ public class Counter {
 		int even = 0;
 		
 		for (int num:list) {
-			if(num % 2 == 0) even=+1;
-		}
-		
-		return even;
-	}
-	
-	public void addNumber(int n) {
-		
-		list.add(n);
-	}
-
-	public ArrayList<Integer> getList() {
-		
-		return list;
-	}
-
-	public void setList1(ArrayList<Integer> list) {
-		
-		this.list = list;
-	}
-	
-}
-package ar.edu.unq.po2.tp3;
-
-import java.util.ArrayList;
-
-public class Counter {
-	
-	private ArrayList<Integer> list= new ArrayList<Integer>();
-	
-	public int getEvenOcurrences() {
-		
-		int even = 0;
-		
-		for (int num:list) {
 			if(num % 2 == 0) even++;
 		}
 		
